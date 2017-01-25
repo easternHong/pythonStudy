c24caf1ab7d53cb3c3c74f3752daff734c8985bd
eastern <463141635@qq.com>
2017-01-25 14:32:59 +0800

    增加例子

---
+++ Pratice/Pratice.py
@@ -0,0 +1,12 @@
+import numpy as np
+import math
+
+def compute():
+    n = 10
+    p = 0.3
+    k = np.arange(0, 21)
+    print(k)
+
+if __name__ == '__main__':
+    print('practice')
+    compute()
